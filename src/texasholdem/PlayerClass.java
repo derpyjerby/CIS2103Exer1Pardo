af7eee49621830c8ceaed6bc8feef4be4f41aa04
pardoje_cis21035 <pardoje_cis21035@ADMINRG-I180BTA.ceaccs.net>
2019-09-11 23:28:48 -0700

    I commit.

---
+++ src/texasholdem/PlayerClass.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package texasholdem;
+
+/**
+ *
+ * @author pardoje_cis21035
+ */
+public class PlayerClass {
+    class Player{
+        String Name;
+        Card Hand[2];
+        int Chips;
+        boolean isDealer;
+    }
+    
+    /*
+    public void giveAnte (int Ant){
+        
+    }
+    
+    public void fold (){
+        
+    }
+    
+    public void check (){
+        
+    }
+    
+    public void raise (int Chips){
+        
+    }
+    
+    public void flipCard (Card rev[2]){
+        
+    }
+    
+    */
+}

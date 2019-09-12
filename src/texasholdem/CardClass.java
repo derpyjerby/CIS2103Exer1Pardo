cc3e0ef5d1fac51b63277fbe7e1133eb51420539
pardoje_cis21035 <pardoje_cis21035@ADMINRG-I180BTA.ceaccs.net>
2019-09-11 23:17:45 -0700

    I commit.

--- src/texasholdem/CardClass.java
+++ src/texasholdem/CardClass.java
@@ -14,11 +14,23 @@ package texasholdem;
 
 
 public class CardClass {
-    
     class Card {
     int face;
     String suit;
-    }
+  
+    /*
+    public void getSuit (String sut){System.out.println(sut);}
     
+    public void getFace (int fez){System.out.println(fez);}
+    
+        public void setSuit (String set){
+         suit = set;
+        }
+        
+        public void setFace (int sat){
+         face = sat;
+        }
+        
+    }
+    */
 }
-
